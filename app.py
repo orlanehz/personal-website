17273250350554c972631ae97381873b034accd9
orlanehz <orlane.houzet@icloud.com>
2021-01-19 22:51:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def about():
 @app.route('/portfolio', methods=['POST', 'GET'])
 def portfolio():
     # get all projects from the database
-    zipped = helper.get_portfolio_content()
+    #zipped = helper.get_portfolio_content()
 
     return render_template('portfolio.html')
                             #projects=zipped)
